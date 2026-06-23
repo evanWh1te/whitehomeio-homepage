bcd66aef1aeae51c1b02cef90d46896800f8f3a5
Evan <evan@whitehome.io>
2026-06-23 00:53:02 -0400

    tweaks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,19 @@
+/** @format */
+
+import type { NuxtConfig } from 'nuxt/schema';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-01-01',
-  devtools: { enabled: true },
-  app: {
-    head: {
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&family=Outfit:wght@300;400;500;600;700;800;900&display=swap',
+export default {
+    compatibilityDate: '2025-01-01',
+    devtools: { enabled: true },
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&family=Outfit:wght@300;400;500;600;700;800;900&display=swap',
+                },
+            ],
         },
-      ],
     },
-  },
-})
+} satisfies NuxtConfig;
